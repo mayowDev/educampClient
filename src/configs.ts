34dd4c3354d13993474990d3e9384f3df6048f7f
mayowDev <mayowdev@gmail.com>
2021-06-20 17:20:01 +0500

    changed local API_url to prod

--- src/configs.ts
+++ src/configs.ts
@@ -1,2 +1,3 @@
-export const BACKEND_URL = 'http://localhost:5000';
+// export const BACKEND_URL = 'http://localhost:5000';
+export const BACKEND_URL = 'https://educampv.herokuapp.com'
 export const STRIPE_PUBLISH_KEY = 'pk_test_51IzMmgBph643tycyzqWr41c02oVDvZZbOl7b54anJ0tkFmycWHjZFhVLnAbghlKaiKgVeS1ajLbR3KiLsuWWnlq200ZTNGFp5I';
