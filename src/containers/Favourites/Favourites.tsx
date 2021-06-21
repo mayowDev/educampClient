c25dd0a22f18859234238c50145fdc2afaccc83c
mayowDev <mayowdev@gmail.com>
2021-06-21 17:02:00 +0500

    make favourites page  dynamic

--- src/containers/Favourites/Favourites.tsx
+++ src/containers/Favourites/Favourites.tsx
@@ -9,8 +9,8 @@ import sampleImage from '../../assets/images/coursesThumbnails/react-thumbnail.j
 const Favourites = (props) => {
     const history = useHistory();
     // const [favourites, setFavorites] = useState<any>(null);
-    const { favouriteItems, getWishlistItems, isLoading, toggleFavouriteExhibition} = props;
-    const [activeTab, setActiveTab] = useState('details');
+    const { favouriteItems, getWishlistItems, toggleFavouriteExhibition} = props;
+    const [activeTab, setActiveTab] = useState('favourites');
 
     // const isFavouritesEmpty = () => {
     //     return favouriteItems&&favouriteItems.length === 0;
@@ -27,30 +27,59 @@ const Favourites = (props) => {
                     <div className="favourites__header">
                         <h2>My Learning</h2>
                         <div className="favourites__header--tabs">
-                            <ul className={`nav-links ${activeTab === "courses" && 'nav-links-favourites'}`}>
-                                <li onClick={() => setActiveTab("courses")}>All Courses</li>
-                                <li onClick={() => setActiveTab("favourites")}>My Wishlist</li>
-                                <li onClick={() => setActiveTab("collections")}>Collections</li>
-                                <li onClick={() => setActiveTab("payments")}>Archived</li>
+                            <ul >
+                                <li onClick={() => setActiveTab("courses")} className={`${activeTab === "courses" ? 'active-link':''}`}>All Courses</li>
+                                <li onClick={() => setActiveTab("favourites")} className={`${activeTab === "favourites" ? 'active-link':''}`}>My Wishlist</li>
+                                <li onClick={() => setActiveTab("collections")} className={`${activeTab === "collections" ? 'active-link':''}`}>Collections</li>
+                                <li onClick={() => setActiveTab("archives")} className={`${activeTab === "archives" ? 'active-link':''}`}>Archived</li>
                             </ul>
                         </div>
                     </div>
                     <ScrollAnimation>
                     <div className="favourites__items" >
-                    {favouriteItems&&favouriteItems.map((data) => {
-                            return(
-                                <div key={data.id} onClick={() => history.push(`/courses/${data.slug}`)} className="favourites__items--item">
-                                    <img src={sampleImage} alt="course-img" />
-                                    <Title2 value={data.title} />
-                                    <h4>{data.createdBy}</h4>
-                                    <div className="price-reviews">
-                                        <Rating/>
-                                        <h3>$89.99</h3>
-                                    </div>
+                        {activeTab ==='courses'&&
+                            <div onClick={() => history.push(`/courses`)} className="favourites__items--item">
+                                <img src={sampleImage} alt="course-img" />
+                                <Title2 value={'MERN Stack Front To Back: Full Stack React & Node.Js With MongoDB'} />
+                                <h4>{'Brad Traversy'}</h4>
+                                <div className="price-reviews">
+                                    <Rating/>
+                                    <h3>$89.99</h3>
                                 </div>
-                            )
-                        })
-                    }
+                            </div>  
+                        }
+                        {activeTab === "favourites" && favouriteItems&&favouriteItems.map((data) => {
+                                return(
+                                    <div key={data.id} onClick={() => history.push(`/courses/${data.slug}`)} className="favourites__items--item">
+                                        <img src={sampleImage} alt="course-img" />
+                                        <Title2 value={data.title} />
+                                        <h4>{data.createdBy}</h4>
+                                        <div className="price-reviews">
+                                            <Rating/>
+                                            <h3>{data.price == 0 ? 'Free' : `$${data.price}.99`}</h3>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                        {activeTab === 'collections'&&
+                            <div className="favourites__items--collections">
+                                <h3>Organize and access your courses faster!</h3>
+                                {/* <h3></h3> */}
+                                <span><Link to="#" onClick={() =>setActiveTab("courses")}>Go to the All Courses tab</Link> to create a collection.</span>
+                            </div> 
+                        }
+                        { activeTab === "archives" &&
+                            <div onClick={() => history.push(`/courses`)} className="favourites__items--item">
+                                <img src={sampleImage} alt="course-img" />
+                                <Title2 value={'MERN Stack Front To Back: Full Stack React & Node.Js With MongoDB'} />
+                                <h4>{'Brad Traversy'}</h4>
+                                <div className="price-reviews">
+                                    <Rating/>
+                                    <h3>$89.99</h3>
+                                </div>
+                            </div>
+                        }
                     </div>
                     </ScrollAnimation>
                     
