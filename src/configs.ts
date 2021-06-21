d081a9e4eaedd801f234f71f17d74b8085933bfb
mayowDev <mayowdev@gmail.com>
2021-06-21 05:16:41 +0500

    build url

--- src/configs.ts
+++ src/configs.ts
@@ -3,8 +3,8 @@
 // console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 
 let API_URL = 'https://educampv.herokuapp.com'; 
-if(process.env.NODE_ENV === 'development'){
-    API_URL ='http://localhost:5000';
-}
+// if(process.env.NODE_ENV === 'development'){
+//     API_URL ='http://localhost:5000';
+// }
 export const BACKEND_URL = API_URL
 export const STRIPE_PUBLISH_KEY = 'pk_test_51IzMmgBph643tycyzqWr41c02oVDvZZbOl7b54anJ0tkFmycWHjZFhVLnAbghlKaiKgVeS1ajLbR3KiLsuWWnlq200ZTNGFp5I';
